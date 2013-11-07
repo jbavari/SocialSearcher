5d756e55c493db5426081b9c0d32991d48ebc8ef
Josh Bavari <josh@raisemore.com>
2013-11-06 21:53:17 -0600

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialSearcher::Application.config.secret_key_base = '313a93808e618cc005b3fc20ff1e5dee73107bdb0c972711ecd40189ee613fbe49937ce5b733afc8013c83c9f09e816a1e2b8431a7701592634a03e4030b5627'
